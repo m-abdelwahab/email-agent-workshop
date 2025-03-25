9243fb0724cae874a03e9687f0a6ed7df3e640c5
Mahmoud Abdelwahab <hey@mahmoud.codes>
2025-03-25 08:41:09 -0600

    update

--- app/lib/db/index.ts
+++ app/lib/db/index.ts
@@ -2,7 +2,7 @@ import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 export * from "drizzle-orm";
 
-export const db = drizzle(process.env.DATABASE_URL, {
-  schema,
-  casing: "snake_case",
-});
+// export const db = drizzle(process.env.DATABASE_URL, {
+//   schema,
+//   casing: "snake_case",
+// });
